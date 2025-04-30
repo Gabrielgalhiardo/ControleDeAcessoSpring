6f9f7b075799ab6e77040839d9ae48e726ce986d
Bia <beatrizandreosbezerra1@gmail.com>
2025-04-30 16:06:23 -0300

    feat: add funcionalidade de adicionar e deletar usuário

--- src/main/java/com/senai/controle_de_acesso_spring/application/service/AlunoService.java
+++ src/main/java/com/senai/controle_de_acesso_spring/application/service/AlunoService.java
@@ -1,57 +1,94 @@
 package com.senai.controle_de_acesso_spring.application.service;
 
 import com.senai.controle_de_acesso_spring.application.dto.users.AlunoDto;
-import com.senai.controle_de_acesso_spring.application.dto.users.UsuarioDto;
-import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.Usuario;
-import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.aluno.Aluno;
-import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDoUsuario;
+import com.senai.controle_de_acesso_spring.domain.model.entity.users.Aluno;
 import com.senai.controle_de_acesso_spring.domain.repository.AlunoRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class AlunoService {
 
-    @Autowired
-    private AlunoRepository alunoRepository;
+    private final AlunoRepository alunoRepository;
 
-    public void cadastrarAluno(AlunoDto alunoDto){
-        alunoRepository.save(alunoDto.fromDTO());
+    public AlunoDto salvarAluno(AlunoDto alunoDto){
+        Aluno aluno = new Aluno();
+        aluno.setNome(alunoDto.nome());
+        aluno.setEmail(alunoDto.email());
+        aluno.setTelefoneCelular(alunoDto.telefoneCelular());
+        aluno.setTelefoneFixo(alunoDto.telefoneFixo());
+        aluno.setCpf(alunoDto.cpf());
+        aluno.setSenha(alunoDto.senha());
+        aluno.setStatusDoUsuario(alunoDto.statusDoUsuario());
+        aluno.setDataDeNascimento(alunoDto.dataDeNascimento());
+        aluno.setIdade(alunoDto.idade());
+//        aluno.setResponsaveisDoAluno(mapResponsaveis(alunoDto.responsaveisDoAluno()));
+//        aluno.setSubTurmas(alunoDto.subTurmas());
+        alunoRepository.save(aluno);
+        return alunoDto;
     }
 
-    public List<AlunoDto> listarAlunosAtivos(){
-       return alunoRepository.findByStatusDoUsuario(StatusDoUsuario.ATIVO).stream().map(AlunoDto::toDTO).collect(Collectors.toList());
+
+    public List<AlunoDto> pegarTodosAlunos(){
+        return alunoRepository.findAll().stream().map(aluno -> new AlunoDto(
+                    aluno.getNome(),
+                    aluno.getEmail(),
+                    aluno.getTelefoneCelular(),
+                    aluno.getTelefoneFixo(),
+                    aluno.getCpf(),
+                    aluno.getSenha(),
+                    aluno.getStatusDoUsuario(),
+                    aluno.getDataDeNascimento(),
+                    aluno.getIdade(),
+                    aluno.getSubTurmas()
+            )).collect(Collectors.toList());
     }
 
-    public Optional<AlunoDto> buscarAlunoPorId(Long id){
-        return alunoRepository.findById(id).filter(a -> a.getStatusDoUsuario().equals(StatusDoUsuario.ATIVO)).map(AlunoDto::toDTO);
+    public AlunoDto buscarAlunoPorId(long id){
+        return alunoRepository.findById(id).map(aluno -> new AlunoDto(
+                    aluno.getNome(),
+                    aluno.getEmail(),
+                    aluno.getTelefoneCelular(),
+                    aluno.getTelefoneFixo(),
+                    aluno.getCpf(),
+                    aluno.getSenha(),
+                    aluno.getStatusDoUsuario(),
+                    aluno.getDataDeNascimento(),
+                    aluno.getIdade(),
+                    aluno.getSubTurmas()
+            )).orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
     }
 
-    public boolean atualizarAluno (Long id, AlunoDto alunoDto){
-        return alunoRepository.findById(id).map(alunoAntigo ->{
-            Aluno alunoAtualizado = alunoDto.fromDTO();
-            alunoAntigo.setNome(alunoAtualizado.getNome());
-            alunoAntigo.setEmail(alunoAtualizado.getEmail());
-            alunoAntigo.setDataNascimento(alunoAtualizado.getDataNascimento());
-            alunoAntigo.setCpf(alunoAtualizado.getCpf());
-            alunoRepository.save(alunoAntigo);
-            return true;
-        }).orElse(false);
-    }
 
-    public boolean inativarAluno(Long id){
-        return alunoRepository.findById(id).map(aluno -> {
-            aluno.setStatusDoUsuario(StatusDoUsuario.INATIVO);
+    public void atualizarAluno(long id, AlunoDto alunoDto) {
+        Aluno aluno = alunoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+            aluno.setTelefoneCelular(alunoDto.telefoneCelular());
+            aluno.setTelefoneFixo(alunoDto.telefoneFixo());
             alunoRepository.save(aluno);
-            return true;
-        }).orElse(false);
-    }
+        }
 
+        public void deletarAluno(long id) {
+            Aluno aluno = alunoRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+            alunoRepository.delete(aluno);
+        }
+
+//    public List<ResponsavelDoAluno> mapResponsaveis(List<ResponsavelDoAlunoDto> responsavelDoAlunoDtos){
+//        return responsavelDoAlunoDtos.stream().map(responsavelDoAlunoDto -> {
+//                    ResponsavelDoAluno responsavelDoAluno = new ResponsavelDoAluno();
+//                    responsavelDoAluno.setNome(responsavelDoAlunoDto.nome());
+//                    responsavelDoAluno.setEmail(responsavelDoAlunoDto.email());
+//                    responsavelDoAluno.setCpf(responsavelDoAlunoDto.cpf());
+//                    responsavelDoAluno.setTelefoneFixo(responsavelDoAlunoDto.telefoneFixo());
+//                    responsavelDoAluno.setTelefoneCelular(responsavelDoAlunoDto.telefoneCelular());
+//            return responsavelDoAluno;
+//        }).collect(Collectors.toList());
+//    }
 
 
 }
