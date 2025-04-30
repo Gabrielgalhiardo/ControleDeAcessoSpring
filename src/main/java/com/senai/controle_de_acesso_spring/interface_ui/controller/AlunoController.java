2bcdc17fb834c32b8089a44d8b3c9bd9df92132d
Bia <beatrizandreosbezerra1@gmail.com>
2025-04-30 16:07:50 -0300

    feat: add método no controller para atualizar e delatar usuário

--- src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/AlunoController.java
+++ src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/AlunoController.java
@@ -35,4 +35,18 @@ public class AlunoController {
         return ResponseEntity.ok(alunos);
     }
 
+    @PutMapping
+    public ResponseEntity<Void> atualizarAluno(@PathVariable Long id, AlunoDto alunoDto) {
+        alunoService.atualizarAluno(id, alunoDto);
+        return ResponseEntity.status(204).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarAluno(@PathVariable Long id) {
+        alunoService.deletarAluno(id);
+        return ResponseEntity.status(204).build();
+    }
+
+
+
 }
