9a6c3405519c1248a73c51e056b356b58d5a985c
IgorLDev <igoralves0003@gmail.com>
2025-04-30 16:05:35 -0300

    feat: Criação dos relacionamentos da classe Aula

--- src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/Aula.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/Aula.java
@@ -1,9 +1,8 @@
 package com.senai.controle_de_acesso_spring.domain.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.senai.controle_de_acesso_spring.domain.model.entity.users.Ambiente;
+import com.senai.controle_de_acesso_spring.domain.model.entity.users.Professor;
+import jakarta.persistence.*;
 
 import java.util.List;
 
@@ -13,5 +12,14 @@ public class Aula {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @OneToOne
+    private UnidadesCurriculares unidadesCurriculares;
+
+    @OneToOne
+    private Professor professor;
+
+    @OneToOne
+    private Ambiente ambiente;
 
 }
