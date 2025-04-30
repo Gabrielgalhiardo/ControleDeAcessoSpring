f5741382a3e1fd4911770e91129e85ec833e4a71
IgorLDev <igoralves0003@gmail.com>
2025-04-30 16:07:29 -0300

    feat: Criação da classe Ocorrencia

--- src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/Ocorrencia.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/Ocorrencia.java
@@ -1,10 +1,11 @@
 package com.senai.controle_de_acesso_spring.domain.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDaOcorrencia;
+import com.senai.controle_de_acesso_spring.domain.model.enums.TipoDoCurso;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -12,4 +13,16 @@ public class Ocorrencia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private String descricao;
+
+    private LocalDateTime dataHora;
+
+    @Enumerated(EnumType.STRING)
+    private StatusDaOcorrencia statusDaOcorrencia;
+
+    @Enumerated(EnumType.STRING)
+    private TipoDoCurso tipoDoCurso;
+
+    //Depois apagar essa mensagem e colocar relacionamento com Aluno
 }
