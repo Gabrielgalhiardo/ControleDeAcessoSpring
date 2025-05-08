c3ad506d42932a3be2ef1c1294ea65f124ee3966
gabriel <galhiardogabriel@gmail.com>
2025-05-08 14:15:28 -0300

    Bug fix:  corrigindo erro de mesclagem na classe ambiente

--- src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/curso/Ambiente.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/curso/Ambiente.java
@@ -8,11 +8,5 @@ public class Ambiente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-<<<<<<< HEAD:src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/Aula.java
-    @ManyToOne
-    private Aula aula;
-}
-=======
     private String nome;
 }
->>>>>>> igor:src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/curso/Ambiente.java
