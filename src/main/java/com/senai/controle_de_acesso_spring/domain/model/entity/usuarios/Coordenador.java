6a8778de3fa9d578a0ebf9e8b049fa3e75fa2501
IgorLDev <igoralves0003@gmail.com>
2025-05-08 17:18:56 -0300

    Refactor: Adicionando equipeDeProfessores na classe Coordenador.

--- src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/usuarios/Coordenador.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/usuarios/Coordenador.java
@@ -2,8 +2,14 @@ package com.senai.controle_de_acesso_spring.domain.model.entity.usuarios;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Data;
+
+import java.util.List;
 
 @Entity
+@Data
 @DiscriminatorValue("COORDENADOR")
 public class Coordenador extends Usuario{
+
+    private List<Professor> equipeProfessores;
 }
