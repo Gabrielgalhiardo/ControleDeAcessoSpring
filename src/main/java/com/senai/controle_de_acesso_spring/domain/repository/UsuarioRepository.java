c963fac94e8cc2a9a49c84727a196aaed06756ec
Eric <ericsouza94895@gmail.com>
2025-05-14 15:23:36 -0300

    feat: add método para buscar os usuários ativos no sistema no repository de usuário

--- src/main/java/com/senai/controle_de_acesso_spring/domain/repository/UsuarioRepository.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/UsuarioRepository.java
@@ -4,6 +4,9 @@ import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findByStatusDoUsuarioATIVO();
 }
