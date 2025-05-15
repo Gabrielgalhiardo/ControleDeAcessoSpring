30a52e721efaf459910be29f4a5a1002aadf8cb6
IgorLDev <igoralves0003@gmail.com>
2025-05-15 17:24:08 -0300

    Feat: Criação do JustificativaRepository

---
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/JustificativaRepository.java
@@ -0,0 +1,13 @@
+package com.senai.controle_de_acesso_spring.domain.repository;
+
+import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.aluno.Justificativa;
+import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDaJustificativa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JustificativaRepository extends JpaRepository<Justificativa, Long> {
+    List<Justificativa> findByStatusDaJustificativa (StatusDaJustificativa statusDaJustificativa);
+}
