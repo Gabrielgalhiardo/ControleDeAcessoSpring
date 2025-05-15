43d945d020844fe450d8c3da3e73a3e886dbc3f7
Beatriz <beatrizandreosbezerra1@gmail.com>
2025-05-15 16:23:15 -0300

    Refactor: Refatorando AdminDto.java e AdminService.java

--- src/main/java/com/senai/controle_de_acesso_spring/domain/repository/AdminRepository.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/AdminRepository.java
@@ -1,10 +1,13 @@
 package com.senai.controle_de_acesso_spring.domain.repository;
 
 import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.Admin;
-import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.Usuario;
+import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    List<Admin> findByStatusDoUsuario(StatusDoUsuario statusDoUsuario);
 }
