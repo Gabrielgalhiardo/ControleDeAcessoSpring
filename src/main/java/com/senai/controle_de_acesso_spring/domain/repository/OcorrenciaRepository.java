fd12d034a7522ebbe2b096b3ae14be7721cbf0d2
Eric <ericsouza94895@gmail.com>
2025-05-15 17:12:55 -0300

    fix: corrigindo alguns erros e adicionando annotations

--- src/main/java/com/senai/controle_de_acesso_spring/domain/repository/OcorrenciaRepository.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/OcorrenciaRepository.java
@@ -1,11 +1,14 @@
 package com.senai.controle_de_acesso_spring.domain.repository;
 
 import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.aluno.Ocorrencia;
+import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDaOcorrencia;
 import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface OcorrenciaRepository extends JpaRepository {
-    List<Ocorrencia> findByStatusDoUsuario (StatusDoUsuario statusDoUsuario);
+@Repository
+public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Long> {
+    List<Ocorrencia> findByStatus(StatusDaOcorrencia statusDaOcorrencia);
 }
