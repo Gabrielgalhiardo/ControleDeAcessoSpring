485b08fde283ae22d2850c0b3062e9bc9814619a
Eric <ericsouza94895@gmail.com>
2025-05-15 17:12:55 -0300

    fix: corrigindo alguns erros e adicionando annotations

--- src/main/java/com/senai/controle_de_acesso_spring/domain/repository/OcorrenciaRepository.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/OcorrenciaRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
 public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Long> {
-    List<Ocorrencia> findByStatusDaOcorrencia (StatusDaOcorrencia statusDaOcorrencia);
+    List<Ocorrencia> findByStatus(StatusDaOcorrencia statusDaOcorrencia);
 }
