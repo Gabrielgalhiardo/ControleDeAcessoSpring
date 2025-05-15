fed0590a4dccc0b96ec21bf423b1225515f68cb8
IgorLDev <igoralves0003@gmail.com>
2025-05-15 17:22:59 -0300

    Fix: Correção dos nomes dos atributos tipoDeJustificativa e StatusDaJustificativa

--- src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/usuarios/aluno/Justificativa.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/model/entity/usuarios/aluno/Justificativa.java
@@ -3,17 +3,19 @@ package com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.aluno;
 import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDaJustificativa;
 import com.senai.controle_de_acesso_spring.domain.model.enums.TipoDeJustificativa;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Justificativa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private TipoDeJustificativa tipo;
+    private TipoDeJustificativa tipoDeJustificativa;
 
     private String descricao;
     private String anexo;
@@ -21,7 +23,7 @@ public class Justificativa {
     private Integer qtdDias;
 
     @Enumerated(EnumType.STRING)
-    private StatusDaJustificativa status;
+    private StatusDaJustificativa statusDaJustificativa;
 
     @ManyToOne
     private Aluno aluno;
