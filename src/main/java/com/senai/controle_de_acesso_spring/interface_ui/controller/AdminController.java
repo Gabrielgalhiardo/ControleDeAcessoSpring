966ecea9fd0c9c1877881a645e63a4b96c135d9b
Beatriz <beatrizandreosbezerra1@gmail.com>
2025-05-15 15:02:00 -0300

    Feat: Adicionando CRUD ao AdminController.java

--- src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/AdminController.java
+++ src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/AdminController.java
@@ -6,10 +6,9 @@ import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.Admin;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -25,4 +24,25 @@ public class AdminController {
         return  ResponseEntity.ok().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizarAdmin(@RequestBody AdminDto adminDto, @PathVariable Long id){
+        adminService.atualizarAdminPeloId(id, adminDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AdminDto>> listarAdmins(){
+        return ResponseEntity.ok().body(adminService.pegarTodosOsAdmin());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AdminDto> listarAdminPeloId(@PathVariable Long id){
+        return ResponseEntity.ok().body(adminService.pegarUmAdminPeloId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarAdminPeloId(@PathVariable Long id){
+        adminService.deletarAdminPeloId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
