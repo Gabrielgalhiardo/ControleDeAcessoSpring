98ecc2dc70cc070cda2be60322a8dd5137780fa9
gabriel <galhiardogabriel@gmail.com>
2025-05-16 16:39:24 -0300

    Fix: atualizando listaOcorrencias e erro no ocorrenciaController

--- src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/OcorrenciaController.java
+++ src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/OcorrenciaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("Ocorrencias")
+@RequestMapping("ocorrencias")
 @AllArgsConstructor
 public class OcorrenciaController {
 
