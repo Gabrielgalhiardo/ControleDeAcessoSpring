924fc37eb7c976276580badb3dd950395d1fea40
Eric <ericsouza94895@gmail.com>
2025-05-21 16:19:53 -0300

    feat: add método para gerar uma ocorrencia de atraso no OcorrenciaService

--- src/main/java/com/senai/controle_de_acesso_spring/application/service/usuarios/aluno/OcorrenciaService.java
+++ src/main/java/com/senai/controle_de_acesso_spring/application/service/usuarios/aluno/OcorrenciaService.java
@@ -1,14 +1,21 @@
 package com.senai.controle_de_acesso_spring.application.service.usuarios.aluno;
 
 import com.senai.controle_de_acesso_spring.application.dto.usuarios.aluno.OcorrenciaDto;
+import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.Usuario;
+import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.aluno.Aluno;
 import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.aluno.Ocorrencia;
 import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDaOcorrencia;
+import com.senai.controle_de_acesso_spring.domain.model.enums.TipoDeOcorrencia;
 import com.senai.controle_de_acesso_spring.domain.repository.OcorrenciaRepository;
+import com.senai.controle_de_acesso_spring.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,12 +24,38 @@ public class OcorrenciaService {
     @Autowired
     private OcorrenciaRepository ocorrenciaRepository;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public void cadastrarOcorrencia(OcorrenciaDto ocorrenciaDto){
         ocorrenciaRepository.save(ocorrenciaDto.fromDTO());
     }
 
     public List<OcorrenciaDto> listarOcorrencias(){
         return ocorrenciaRepository.findAll().stream().map(OcorrenciaDto::toDTO).collect(Collectors.toList());
+    }
+
+    public String criarOcorrenciaDeAtraso(String idAcesso) {
+        Optional<Usuario> usuario = usuarioRepository.findByIdAcesso(idAcesso);
+        if (usuario.isPresent()) {
+            if (usuario.get() instanceof Aluno aluno){
+                Ocorrencia ocorrencia = new Ocorrencia();
+                ocorrencia.setTipo(TipoDeOcorrencia.ATRASO);
+                ocorrencia.setDescricao("Atraso na entrada");
+                ocorrencia.setStatusDaOcorrencia(StatusDaOcorrencia.AGUARDANDO_AUTORIZACAO);
+                ocorrencia.setDataHoraCriacao(LocalDateTime.now());
+                ocorrencia.setAluno(aluno);
+                ocorrenciaRepository.save(ocorrencia);
+                System.out.println("Ocorrência de atraso criada com sucesso!");
+                return "Ocorrência de atraso criada com sucesso!";
+            } else {
+                System.out.println("Usuário não é um aluno");
+                throw new RuntimeException("Usuário não é um aluno");
+            }
+        } else {
+            System.out.println("Usuário não encontrado");
+            throw new RuntimeException("Usuário não encontrado");
+        }
     }
 
     public Optional<OcorrenciaDto> buscarOcorrenciaPorId(Long id){
