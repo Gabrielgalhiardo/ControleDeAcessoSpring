ed25d78030ff8b9c08a156552767032e76fa8da1
Eric <ericsouza94895@gmail.com>
2025-05-21 16:16:42 -0300

    feat: add método para buscar usuários pelo id de acesso

--- src/main/java/com/senai/controle_de_acesso_spring/domain/repository/AlunoRepository.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/AlunoRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
     List<Aluno> findByStatusDoUsuario(StatusDoUsuario statusDoUsuario);
+    Optional<Aluno> findByIdAcesso(String idAcesso);
 }
