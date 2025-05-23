5127f93b44e0e3758e198dd1bcee95b8ce824c1f
Eric <ericsouza94895@gmail.com>
2025-05-23 15:32:57 -0300

    feat: add DTO da entidade Semestre

---
+++ src/main/java/com/senai/controle_de_acesso_spring/application/dto/turma/SemestreDTO.java
@@ -0,0 +1,17 @@
+package com.senai.controle_de_acesso_spring.application.dto.turma;
+
+import com.senai.controle_de_acesso_spring.domain.model.entity.turma.Semestre;
+
+public record SemestreDTO (
+        Long id,
+        int numero,
+        String nomeDaTurma
+) {
+    public static SemestreDTO toDTO(Semestre semestre) {
+        return new SemestreDTO(
+                semestre.getId(),
+                semestre.getNumero(),
+                semestre.getNomeDaTurma()
+        );
+    }
+}
