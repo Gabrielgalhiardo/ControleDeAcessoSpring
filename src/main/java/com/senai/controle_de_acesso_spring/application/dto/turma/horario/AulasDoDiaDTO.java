bf161cce21412aab52a6eb140b6cc244fa49d06f
Eric <ericsouza94895@gmail.com>
2025-05-23 15:31:56 -0300

    feat: add DTO da entidade AulasDoDia

---
+++ src/main/java/com/senai/controle_de_acesso_spring/application/dto/turma/horario/AulasDoDiaDTO.java
@@ -0,0 +1,27 @@
+package com.senai.controle_de_acesso_spring.application.dto.turma.horario;
+
+import com.senai.controle_de_acesso_spring.domain.model.entity.turma.horarios.AulasDoDia;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+public record AulasDoDiaDTO(
+        Long id,
+        DayOfWeek diaDaSemana,
+        List<AulaDTO> aulas
+) {
+    public static AulasDoDiaDTO toDTO(AulasDoDia d) {
+        return new AulasDoDiaDTO(
+                d.getId(),
+                d.getDiaDaSemana(),
+                d.getAulas().stream().map(AulaDTO::toDTO).toList()
+        );
+    }
+
+    public AulasDoDia fromDTO() {
+        AulasDoDia dia = new AulasDoDia();
+        dia.setDiaDaSemana(diaDaSemana);
+        dia.setAulas(aulas.stream().map(AulaDTO::fromDTO).toList());
+        return dia;
+    }
+}
