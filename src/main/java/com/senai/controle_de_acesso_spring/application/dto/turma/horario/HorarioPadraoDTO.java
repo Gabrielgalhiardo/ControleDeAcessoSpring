11d1a5cbd973fc7ac7b56ed6c2d14d6ec97c3481
Eric <ericsouza94895@gmail.com>
2025-05-23 15:32:46 -0300

    feat: add DTO da entidade HorarioPadrao

---
+++ src/main/java/com/senai/controle_de_acesso_spring/application/dto/turma/horario/HorarioPadraoDTO.java
@@ -0,0 +1,25 @@
+package com.senai.controle_de_acesso_spring.application.dto.turma.horario;
+
+import com.senai.controle_de_acesso_spring.domain.model.entity.turma.horarios.HorarioPadrao;
+
+import java.util.List;
+
+public record HorarioPadraoDTO(
+        Long id,
+        List<AulasDoDiaDTO> listaDeAulasDoDia
+) {
+    public static HorarioPadraoDTO toDTO(HorarioPadrao horario) {
+        return new HorarioPadraoDTO(
+                horario.getId(),
+                horario.getListaDeAulasDoDia().stream().map(AulasDoDiaDTO::toDTO).toList()
+        );
+    }
+
+    public HorarioPadrao fromDTO() {
+        HorarioPadrao horario = new HorarioPadrao();
+        horario.setListaDeAulasDoDia(
+                listaDeAulasDoDia.stream().map(AulasDoDiaDTO::fromDTO).toList()
+        );
+        return horario;
+    }
+}
