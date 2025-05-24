7329726af57d523c8df7d21521f8827bf2d3a886
Eric <ericsouza94895@gmail.com>
2025-05-23 23:00:22 -0300

    feat; add repositorio da entidade Aula

---
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/turma/horarios/AulaRepository.java
@@ -0,0 +1,9 @@
+package com.senai.controle_de_acesso_spring.domain.repository.turma.horarios;
+
+import com.senai.controle_de_acesso_spring.domain.model.entity.turma.horarios.Aula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AulaRepository extends JpaRepository<Aula, Long> {
+}
