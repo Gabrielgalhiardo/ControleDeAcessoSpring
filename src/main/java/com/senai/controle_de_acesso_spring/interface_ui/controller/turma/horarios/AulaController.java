6c850d156061a4a8009dced40d3e63adf8441d59
Gugdit <gugudittricht@gmail.com>
2025-06-12 16:15:10 -0300

    feat: arrumando alguns erros

--- src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/turma/horarios/AulaController.java
+++ src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/turma/horarios/AulaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aula")
+@RequestMapping("/aulas")
 public class AulaController {
     @Autowired
     AulaService aulaService;
