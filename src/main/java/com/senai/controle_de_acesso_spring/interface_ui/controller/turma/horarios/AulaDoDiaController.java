ef103719be7b349f12d01f966f2e4700c26f7feb
gabriel <galhiardogabriel@gmail.com>
2025-06-12 16:38:17 -0300

    refactor: endpoint AulaDoDiaController para /aulas-do-dia

--- src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/turma/horarios/AulaDoDiaController.java
+++ src/main/java/com/senai/controle_de_acesso_spring/interface_ui/controller/turma/horarios/AulaDoDiaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aulasDoDia")
+@RequestMapping("/aulas-do-dia")
 public class AulaDoDiaController {
     @Autowired
     AulasDoDiaService aulasDoDiaService;
