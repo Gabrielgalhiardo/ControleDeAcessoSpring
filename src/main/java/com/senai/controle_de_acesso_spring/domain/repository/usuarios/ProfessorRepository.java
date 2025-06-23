f3687495198aa5211953b9fdd09fb522da08e464
Eric <ericsouza94895@gmail.com>
2025-06-22 23:59:23 -0300

    perf: add método para buscar professores com mais performance

--- src/main/java/com/senai/controle_de_acesso_spring/domain/repository/usuarios/ProfessorRepository.java
+++ src/main/java/com/senai/controle_de_acesso_spring/domain/repository/usuarios/ProfessorRepository.java
@@ -2,12 +2,17 @@ package com.senai.controle_de_acesso_spring.domain.repository.usuarios;
 
 import com.senai.controle_de_acesso_spring.domain.model.entity.usuarios.Professor;
 import com.senai.controle_de_acesso_spring.domain.model.enums.StatusDoUsuario;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
     List<Professor> findByStatusDoUsuario(StatusDoUsuario statusDoUsuario);
+    @EntityGraph(attributePaths = {"permissoes"})
+    List<Professor> findAllByIdIn(Collection<Long> ids);
+
 }
